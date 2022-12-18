0aa7a934151fc304216249ef05713bf9d465e387
JavierRibaldelRio <javierribal@gmail.com>
2022-12-18 22:28:49 +0100

    He empezado a trabajar en la web

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import BookForm from './components/BookForm';
+
+import Button from 'react-bootstrap/Button'
+function App() {
+  return (
+    <div>
+      <BookForm />
+
+    </div>
+  );
+}
+
+export default App;
