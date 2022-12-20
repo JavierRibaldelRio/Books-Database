b08d97930a30e7ef78fb01a4347a7b32d91ce212
JavierRibaldelRio <javierribal@gmail.com>
2022-12-21 00:07:57 +0100

    Inicio configuracion SQL en Flask

--- backend/app.py
+++ backend/app.py
@@ -1,14 +1,26 @@
 # Servidor
-
+import os
 #Importa la librerias de flask
 
-from flask import Flask, flash, redirect, render_template, request, session
+from flask import Flask
+
+#Importa las librerias de sqlAclchemy
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.sql import text
+from models.libro import Libro
+
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
 
 @app.route('/api/add-book', methods=['GET','POST'])
 def add_book():
 
-    print(request.form.get('autor'))
+    libro = Libro('Ender','JK','CAste',"23",'24')
+    
 
-    return "Hi World"
+    return "Hi World" 
