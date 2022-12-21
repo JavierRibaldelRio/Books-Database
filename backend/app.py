552ce8e59f646ff40ea698c463eed0008f7cd9ca
JavierRibaldelRio <javierribal@gmail.com>
2022-12-21 18:13:34 +0100

    He conseguido conectar la base de datos al server

--- backend/app.py
+++ backend/app.py
@@ -7,20 +7,21 @@ from flask import Flask
 #Importa las librerias de sqlAclchemy
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.sql import text
-from models.libro import Libro
-
+from models.models import Libros, db
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///../database.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-db = SQLAlchemy(app)
+db.init_app(app)
 
 @app.route('/api/add-book', methods=['GET','POST'])
 def add_book():
 
-    libro = Libro('Ender','JK','CAste',"23",'24')
+    libros = Libros(titulo='Ender',idioma='CAste',fecha_inicio="23",fecha_finalizacion='24',autor_id=12)
+
+    libros.save()
     
 
     return "Hi World" 
---
+++ backend/models/models.py
@@ -0,0 +1,27 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+#Define la clase libro
+
+class Libros(db.Model):
+    libro_id = db.Column(db.Integer, primary_key=True)
+    titulo = db.Column(db.String)
+    idioma = db.Column(db.String)
+    fecha_inicio = db.Column(db.String)
+    fecha_finalizacion = db.Column(db.String)
+    autor_id = db.Column(db.Integer)
+
+    def __init__(self, titulo, idioma, fecha_inicio, fecha_finalizacion, autor_id):
+        self.titulo = titulo
+        self.idioma = idioma
+        self.fecha_inicio = fecha_inicio
+        self.fecha_finalizacion = fecha_finalizacion
+        self.autor_id = autor_id
+
+    # Función que guarda el objeto
+    def save(self):
+        if not self.libro_id:
+            db.session.add(self)
+        db.session.commit()
+    pass
+
