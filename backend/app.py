fee481723c14bb36f5aa11dc378effff2273ce85
JavierRibaldelRio <javierribal@gmail.com>
2022-12-22 22:52:47 +0100

    He avanzado con el servidor

--- backend/app.py
+++ backend/app.py
@@ -2,31 +2,36 @@
 import os
 #Importa la librerias de flask
 
-from flask import Flask
+from flask import Flask, request;
 
 #Importa las librerias de sqlAclchemy
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.sql import text
-from models.models import Libros, db
+from models.models import Libros, db, Autores
+
+
 from scripts.recibirformlibro import recibir_form_libro
 
+#Configura la app
 app = Flask(__name__)
 
+#Configua la base de datos
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///../database.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db.init_app(app)
 
+# Recibe desde un formulario para crear un nuevo libro
 @app.route('/api/add-book', methods=['GET','POST'])
 def add_book():
 
+
     #Obtiene las variables de el cliente
-    print(request.form.get())
-    libro = recibir_form_libro(request)
+    libro = recibir_form_libro(request);
+print(Autores.query.filter_by(autor_id=autor_id).first())
 
-    # libros = Libros(titulo='Ender',idioma='CAste',fecha_inicio="23",fecha_finalizacion='24',autor_id=12)
+    libro.save()
 
-    libros.save()
     
 
     return "Hi World" 
