5c7435e2198743fc6e07955644611a59b335e32c
JavierRibaldelRio <javierribal@gmail.com>
2023-01-02 23:43:55 +0100

    Cambios de desarollo

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,9 +2,13 @@ import './App.css';
 import BookForm from './components/BookForm';
 
 import Button from 'react-bootstrap/Button'
+import MostrarLibros from './components/MostrarLibros';
 function App() {
   return (
     <div>
+
+      <MostrarLibros></MostrarLibros>
+
       <BookForm />
     </div>
   );
