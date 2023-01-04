5ac46b22a73e5bff5f1db1a5b41269222d61d63a
JavierRibaldelRio <javierribal@gmail.com>
2023-01-04 22:54:53 +0100

    He eliminado un import inecessario

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,6 @@
 import './App.css';
 import BookForm from './components/BookForm';
 
-import Button from 'react-bootstrap/Button'
 import MostrarLibros from './components/MostrarLibros';
 function App() {
   return (
