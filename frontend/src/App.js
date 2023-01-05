369db161561191f12ebd934948906bc2440126a7
JavierRibaldelRio <javierribal@gmail.com>
2023-01-05 18:04:58 +0100

    He añadido la parte de añadir libros

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,14 @@
-import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import Main from './pages/Main';
 import AddBook from './pages/Add-Book';
+
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-
-        <Route path='/' element={<Main />} >
-          <Route path='/add-book' element={<AddBook />} />
-
-        </Route >
-
+        <Route path="/" element={<Main />} />
+        <Route path="/add-book" element={<AddBook />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ frontend/src/pages/Add-Book.js
@@ -0,0 +1,20 @@
+// Añade el libro a la base de datos
+
+import BookForm from "../components/BookForm";
+
+function AddBook() {
+
+
+    return <>
+
+        <h2>Añadir Libro</h2>
+
+        <hr />
+        <br />
+
+        <BookForm />
+
+    </>;
+}
+
+export default AddBook;
