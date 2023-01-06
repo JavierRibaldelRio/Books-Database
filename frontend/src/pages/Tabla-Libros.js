a9c4e88a1152ee7b3a0b0cb284c43f0d6efb548a
JavierRibaldelRio <javierribal@gmail.com>
2023-01-06 17:27:35 +0100

    He añadido la tabla libros

---
+++ frontend/src/pages/Tabla-Libros.js
@@ -0,0 +1,14 @@
+import MostrarLibros from '../components/MostrarLibros';
+
+function TablaLibros() {
+
+    return <>
+        <h2>TABLA LIBROS</h2>
+        <hr></hr>
+        <br />
+
+        <MostrarLibros />
+    </>
+}
+
+export default TablaLibros;
