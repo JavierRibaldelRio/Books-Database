4e58082eef2796bb9552967e931a0dae530c619e
JavierRibaldelRio <javierribal@gmail.com>
2023-01-07 12:19:01 +0100

    He conectado la tabla con mostra libro

--- frontend/src/components/FilaLibros.js
+++ frontend/src/components/FilaLibros.js
@@ -3,6 +3,8 @@ import { pasarAMayusFrase } from "../scripts/pasarAMayus";
 
 import transformarFecha from "../scripts/transformarFecha";
 
+import { Link } from 'react-router-dom';
+
 function FilaLibros(props) {
 
     const { libro_id, autor, fecha_inicio, fecha_finalizacion, idioma, titulo } = props.libro;
@@ -14,7 +16,11 @@ function FilaLibros(props) {
 
     return <tr>
         <td>{libro_id}</td>
-        <td>{pasarAMayusFrase(titulo)}</td>
+        <td title="+ Mas info" className="link">
+            <Link to={'../' + libro_id}>
+                {pasarAMayusFrase(titulo)}
+
+            </Link></td>
         <td>{pasarAMayusFrase(autor)}</td>
         <td>{pasarAMayusFrase(idioma)}</td>
         <td>{fi}</td>
