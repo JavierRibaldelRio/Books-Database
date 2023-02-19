8bd005a89392facde34add84a1cc981adaf5c8ed
JavierRibaldelRio <javierribal@gmail.com>
2023-02-19 23:07:01 +0100

    Adaptación zona añadir libro a nuevo formulario

--- frontend/src/pages/Add-Book.js
+++ frontend/src/pages/Add-Book.js
@@ -1,6 +1,7 @@
 // Añade el libro a la base de datos
 
 import BookForm from "../components/BookForm";
+import { faPlus } from "@fortawesome/free-solid-svg-icons";
 import Titulo from "../components/Titulo";
 
 function AddBook() {
@@ -10,7 +11,7 @@ function AddBook() {
 
         <Titulo text="AÑADIR LIBRO" />
 
-        <BookForm />
+        <BookForm text="Añadir libro" ruta='/api/add-book' ico={faPlus} />
 
     </>;
 }
