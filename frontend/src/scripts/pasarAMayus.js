e425c451890d8fe5b281d4c41860d8626c99312a
JavierRibaldelRio <javierribal@gmail.com>
2023-03-13 19:24:42 +0100

    Solución Bug

--- frontend/src/scripts/pasarAMayus.js
+++ frontend/src/scripts/pasarAMayus.js
@@ -1,9 +1,9 @@
 //Se ocupa de pasar a mayúsculas la primer letra de una palabra
 
-var pasarAMayusPalabra = (str) => (str[0] !== undefined) ? str[0].toUpperCase() + str.substring(1) : undefined;
+var pasarAMayusPalabra = (str) => (str !== undefined && str[0] !== undefined) ? str[0].toUpperCase() + str.substring(1) : undefined;
 
 //Pasa una frase con cada letra de una palabra a mayúsculas
-var pasarAMayusFrase = (str) => (str[0] !== undefined) ? str.trim().split(' ').map(pasarAMayusPalabra).join(' ') : undefined;
+var pasarAMayusFrase = (str) => (str !== undefined && str[0] !== undefined) ? str.trim().split(' ').map(pasarAMayusPalabra).join(' ') : undefined;
 
 
 export default pasarAMayusPalabra;
