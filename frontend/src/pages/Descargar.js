26ebf44e91b03752d883a91a09e428938b03d1d6
JavierRibaldelRio <javierribal@gmail.com>
2023-03-13 19:29:28 +0100

    Página de descarga

--- frontend/src/pages/Descargar.js
+++ frontend/src/pages/Descargar.js
@@ -1,7 +1,27 @@
 // Página del cliente para descargar
 
+import Titulo from "../components/Titulo";
+
+import { faArrowDown, faDatabase } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 function Descargar() {
 
+
+    //Habrá qu ecambiarlo en un fututo
+    let proxy = "http://127.0.0.1:5000";
+    return <>
+        <Titulo text="Descargar Datos"></Titulo>
+
+
+        <div id="botones-mostrar" style={{ width: 50 + "%" }} >
+
+            <a role="button" className="btn btn-primary" href={proxy + "/download-json"}>Descargar JSON &nbsp;<FontAwesomeIcon icon={faArrowDown} /></a><br />
+
+            <a role="button" className="btn btn-secondary" href={proxy + "/download-db"}>Descargar DB &nbsp;<FontAwesomeIcon icon={faDatabase} /></a>
+
+        </div>
+    </>
 }
 
 export default Descargar;
