4cc6496b6a786350e7ce94039ff43a1f7fa5ea74
JavierRibaldelRio <javierribal@gmail.com>
2023-03-13 18:36:51 +0100

    He creado la página de descargar y la he añadido a la web

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,14 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './index.css';
+
+//Sub páginas
 import Main from './pages/Main';
 import AddBook from './pages/Add-Book';
 import TablaLibros from './pages/Tabla-Libros'
 import MostrarLibro from './pages/MostrarLibro';
 import Editar from './pages/Editar';
 import Buscar from './pages/Buscar'
+import Descargar from './pages/Descargar';
 
 function App() {
   return (
@@ -17,7 +20,7 @@ function App() {
         <Route path='/editar' element={<Editar />} />
         <Route path='/:id' element={<MostrarLibro />} />
         <Route path='/buscar' element={<Buscar />} />
-
+        <Route path='/descargar' element={<Descargar />} />
       </Routes>
 
       <a href='/tabla-libros'>TABLA</a>
---
+++ frontend/src/pages/Descargar.js
@@ -0,0 +1,7 @@
+// Página del cliente para descargar
+
+function Descargar() {
+
+}
+
+export default Descargar;
