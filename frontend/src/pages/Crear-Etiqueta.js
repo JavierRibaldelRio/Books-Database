55deb68fb6df3a6308e8ea422194d0dea4c75507
JavierRibaldelRio <javierribal@gmail.com>
2023-03-14 23:46:44 +0100

    Creación página etiqueta

---
+++ frontend/src/pages/Crear-Etiqueta.js
@@ -0,0 +1,17 @@
+
+import EtiquetaForm from '../components/EtiquetaForm';
+import Titulo from '../components/Titulo'
+
+// Página que crea una etiqueta
+
+function CrearEtiqueta() {
+
+
+    return <>
+        <Titulo text="Crear Nueva Etiqueta" />
+
+        <EtiquetaForm />
+    </>
+}
+
+export default CrearEtiqueta;
