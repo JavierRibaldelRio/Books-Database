2abea95ceb2d55148b566a2af2f0dc68e4bc1eae
JavierRibaldelRio <javierribal@gmail.com>
2023-03-15 13:14:33 +0100

    Creación de función que genera color aleatorio

---
+++ frontend/src/scripts/RandomHex.js
@@ -0,0 +1,16 @@
+// Genera un color random
+function RandomHex() {
+
+    //Dodos los posibles valorea que puede tener el color
+    const ch = "0123456789ABCDEF";
+
+    let col = "#"       //Caracter inicial del color
+
+    // Genera el nuevo color
+    for (var i = 0; i < 6; i++) {
+        //Nuevo valor del color
+        col += ch[Math.floor(Math.random() * 16)]
+    }
+
+    return col;
+}
