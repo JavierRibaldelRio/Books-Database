3e17e4289448099f9a9fcbf9867dd4f502bee1bb
JavierRibaldelRio <javierribal@gmail.com>
2023-03-19 13:47:01 +0100

    Comentarios + Default Props

--- frontend/src/components/AlertaCerrable.js
+++ frontend/src/components/AlertaCerrable.js
@@ -1,20 +1,30 @@
 import React, { useState } from 'react';
 import Alert from 'react-bootstrap/Alert';
 
-
+//Crea una alerta que se pude cerrar
 function AlertaCerrable(props) {
 
+    // Crea un estado
     let [mostrar, setMostrar] = useState(true);
-
-    const { tipo, texto } = props
-
+    // Obtone las propiedades
+    const { tipo, texto } = props;
+    // Devulve la alerta
     return <Alert key={tipo} variant={tipo} show={mostrar} dismissible onClose={() => setMostrar(false)}>
 
         {texto}
-    </Alert>
+    </Alert>;
 
 
 }
+
+// Crea las propiedades por defecto
+AlertaCerrable.defaultProps = {
+
+
+    tipo: 'primary',
+
+    texto: ""
+};
 
 
 export default AlertaCerrable;
