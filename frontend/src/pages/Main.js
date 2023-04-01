bd3f131d352b98ff30bb17f01ec4e8b1ff24c5f8
JavierRibaldelRio <javierribal@gmail.com>
2023-04-01 17:33:27 +0200

    Creación de componente gráfico de sectores

--- frontend/src/pages/Main.js
+++ frontend/src/pages/Main.js
@@ -1,8 +1,18 @@
 // Es la página principal de la web de book database
 
+import GraficoSectores from "../components/D3/GraficoSectores";
+
 function Main() {
 
-    return <h2>HOLADF DFSDDF</h2>;
+
+
+    const data = { castellano: 12, ruso: 17, valenciano: 5 }
+
+    return <><h2>HOLAssDF DFSDDF</h2>
+
+        <GraficoSectores width={450} height={450} margin={40} data={data} ruta="/api/data/fetch-idiomas" />
+
+    </>;
 }
 
 export default Main;
