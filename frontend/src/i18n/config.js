db9d3bbff716ac33da693a7356801041014914e0
JavierRibaldelRio <javierribal@gmail.com>
2023-04-10 19:18:42 +0200

    Configuración e importar en index i18n

---
+++ frontend/src/i18n/config.js
@@ -0,0 +1,26 @@
+// Archivo que configura las traducciones
+// https://www.freecodecamp.org/news/how-to-add-localization-to-your-react-app/
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+
+i18n.use(initReactI18next).init({
+
+    fallbackLng: 'en',
+    lng: 'en',
+    resources: {
+        en: {
+            translations: require('./locales/en/translations.json')
+        },
+        es: {
+            translations: require('./locales/es/translations.json')
+        }
+    },
+    ns: ['translations'],
+    defaultNS: 'translations'
+
+});
+
+i18n.languages = ["en", "es"];
+
+
+export default i18n;
