e9c27c80855c10c20015e8e6ef1e137291a1f1cb
JavierRibaldelRio <javierribal@gmail.com>
2023-04-13 16:30:34 +0200

    Traducción Valenciano

--- frontend/src/i18n/config.js
+++ frontend/src/i18n/config.js
@@ -6,13 +6,16 @@ import { initReactI18next } from 'react-i18next';
 i18n.use(initReactI18next).init({
 
     fallbackLng: 'es',
-    lng: 'es',
+    lng: 'cat',
     resources: {
         en: {
             translations: require('./locales/en/translations.json')
         },
         es: {
             translations: require('./locales/es/translations.json')
+        },
+        cat: {
+            translations: require('./locales/cat/translations.json')
         }
     },
     ns: ['translations'],
