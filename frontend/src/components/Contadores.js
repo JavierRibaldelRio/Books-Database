35f086d65c9395d5ab6400ad4e57ab2dbe50650d
Javier Ribal del Río <javierribal@gmail.com>
2023-06-18 10:50:36 +0200

    Solución Bug Locale

--- frontend/src/components/Contadores.js
+++ frontend/src/components/Contadores.js
@@ -20,9 +20,9 @@ function Contadores() {
     return <>
 
         <ul id='contadores'>
-            <li><strong>{t("libros")}: </strong>{data.libros.toLocaleString()}</li>
-            <li><strong>{t("colecciones")}: </strong>{data.colecciones.toLocaleString()}</li>
-            <li><strong>{t("media-dias-libro")}: </strong>{data.media.toLocaleString()}</li>
+            <li><strong>{t("libros")}: </strong>{Number(data.libros).toLocaleString() || ''}</li>
+            <li><strong>{t("colecciones")}: </strong>{Number(data.colecciones).toLocaleString() || ''}</li>
+            <li><strong>{t("media-dias-libro")}: </strong>{Number(data.media).toLocaleString() || ''}</li>
         </ul></>
 
 }
