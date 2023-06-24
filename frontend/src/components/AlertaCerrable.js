c0c9558c1846853aaf8c1867f4bb3b0e0c6178eb
Javier Ribal del Río <javierribal@gmail.com>
2023-06-24 11:06:38 +0200

    AlertaCerrable.js actualiza state por cambio props

--- frontend/src/components/AlertaCerrable.js
+++ frontend/src/components/AlertaCerrable.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Alert from 'react-bootstrap/Alert';
 import Alerta from '../classes/Alerta';
 
@@ -8,8 +8,13 @@ function AlertaCerrable(props) {
 
     // Crea un estado
     let [mostrar, setMostrar] = useState(props.alerta.mostrar);
-    // Obtone las propiedades
+
+    // Ponemos props.alerta en vex de props.alerta mostrar, porque nos interesa modificar el estado cuando cambie el texto
+    useEffect(() => setMostrar(props.alerta.mostrar), [props.alerta]);
+
+    // Obtiene las propiedades
     const { tipo, texto } = props.alerta;
+
     // Devulve la alerta
     return <Alert className='alerta-cerrable' key={tipo} variant={tipo} show={mostrar} dismissible onClose={() => setMostrar(false)}>
 
