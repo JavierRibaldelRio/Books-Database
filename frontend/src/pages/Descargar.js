fe3b71c8d891fdf59adc33d07bd39bf23e01bf6f
Javier Ribal del Río <javierribal@gmail.com>
2023-06-25 13:56:50 +0200

    Botones de descargar por Fetch

--- frontend/src/pages/Descargar.js
+++ frontend/src/pages/Descargar.js
@@ -6,22 +6,32 @@ import { faArrowDown, faDatabase } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useTranslation } from "react-i18next";
 
-
+// Muestra los botones para descargar
 function Descargar() {
 
     const { t } = useTranslation();
 
-    //Habrá qu ecambiarlo en un fututo
-    let proxy = "http://127.0.0.1:5000";
+    // Funciones de descarga
+    const handleClickJson = () => fetch('/download-json').then(res => res.blob())
+        .then(blob => {
+            let file = window.URL.createObjectURL(blob);
+            window.location.assign(file);
+        });
+
+    const handleClickDB = () => fetch('/download-db').then(res => res.blob())
+        .then(blob => {
+            let file = window.URL.createObjectURL(blob);
+            window.location.assign(file);
+        });
+
     return <>
         <Titulo text={t("descargar-datos")}></Titulo>
 
 
         <div id="botones-mostrar" style={{ width: 50 + "%" }} >
 
-            <a role="button" className="btn btn-primary" href={proxy + "/download-json"}>{t("descargar-json")} &nbsp;<FontAwesomeIcon icon={faArrowDown} /></a><br />
-
-            <a role="button" className="btn btn-secondary" href={proxy + "/download-db"}>{t("descargar-db")} &nbsp;<FontAwesomeIcon icon={faDatabase} /></a>
+            <button className="btn btn-primary" onClick={handleClickJson}>{t("descargar-json")} &nbsp;<FontAwesomeIcon icon={faArrowDown} /></button><br />
+            <button className="btn btn-secondary" onClick={handleClickDB}>{t("descargar-db")} &nbsp;<FontAwesomeIcon icon={faDatabase} /></button>
 
         </div>
     </>
